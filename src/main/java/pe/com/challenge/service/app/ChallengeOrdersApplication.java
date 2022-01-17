dcfaf1c337af13de21498aa15bfe76d3e53e9426
Jorge Chilcon <jorge.chilcon@complexless.com>
2022-01-17 17:23:50 -0500

    first commit

---
+++ src/main/java/pe/com/challenge/service/app/ChallengeOrdersApplication.java
@@ -0,0 +1,13 @@
+package pe.com.challenge.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/challenge/service/app/ChallengeOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.challenge.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
